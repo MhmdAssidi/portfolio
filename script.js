bf131a8cd3fe8d2838a688580c0c71226aa42176
Mohammad Assidi <mhmdassidi2002@egmail.com>
2025-04-10 13:39:09 +0300

    Designing the summary section

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
+window.addEventListener('scroll', function () {
+  const navbar = document.querySelector('.navbar');
 
-  document.addEventListener("DOMContentLoaded", function () {
-    const typedText = document.querySelector('.hero .hero-text .typed-text');
-    const output = document.querySelector('.hero .hero-text .typing-output');
+  // Add the nav-sticky class when scrolling
+  if (window.scrollY > 0) {
+    navbar.classList.add('nav-sticky');
+  } else {
+    navbar.classList.remove('nav-sticky');
+  }
+});
 
-    if (typedText && output) {
-      const strings = typedText.textContent.split(', ').map(str => str.trim());
+document.addEventListener("DOMContentLoaded", function () {
+  const navbar = document.querySelector('.navbar');
+  if (window.scrollY > 0) {
+    navbar.classList.add('nav-sticky');
+  }
+});
 
-      new Typed(output, {
-        strings: strings,
-        typeSpeed: 100,
-        backSpeed: 50,
-        backDelay: 1500,
-        loop: true,
-        smartBackspace: false,
-        showCursor: true,
-        cursorChar: '|'
-      });
-    }
-  });
+
+  const typedText = document.querySelector('.hero .hero-text .typed-text');
+  const output = document.querySelector('.hero .hero-text .typing-output');
+  if (typedText && output) {
+    const strings = typedText.textContent.split(', ').map(str => str.trim());
+
+    new Typed(output, {
+      strings: strings,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 1500,
+      loop: true,
+      smartBackspace: false,
+      showCursor: true,
+      cursorChar: '|',
+    });
+  }
